2e1735cbc3c4f5440e1d052606d08ceb644cb4ab
Gandalf329 <pohlebaev.n@yandex.ru>
2022-05-14 18:45:15 +0300

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,252 @@
+﻿namespace Chemistry1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown3 = new System.Windows.Forms.NumericUpDown();
+            this.button6 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(134, 43);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(245, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(134, 43);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(478, 12);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(134, 43);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
+            this.button4.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.button4.Location = new System.Drawing.Point(635, 387);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(153, 51);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "Сброс";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 112);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox1.Size = new System.Drawing.Size(216, 122);
+            this.textBox1.TabIndex = 4;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(245, 112);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(5, 3, 3, 3);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox2.Size = new System.Drawing.Size(216, 122);
+            this.textBox2.TabIndex = 5;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(478, 112);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox3.Size = new System.Drawing.Size(216, 122);
+            this.textBox3.TabIndex = 6;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(52, 252);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(94, 29);
+            this.button5.TabIndex = 7;
+            this.button5.Text = "Copy1";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(518, 252);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(94, 29);
+            this.button7.TabIndex = 9;
+            this.button7.Text = "Copy3";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(12, 70);
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(134, 27);
+            this.numericUpDown1.TabIndex = 10;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown1.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
+            // 
+            // numericUpDown2
+            // 
+            this.numericUpDown2.Location = new System.Drawing.Point(245, 70);
+            this.numericUpDown2.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown2.Name = "numericUpDown2";
+            this.numericUpDown2.Size = new System.Drawing.Size(134, 27);
+            this.numericUpDown2.TabIndex = 11;
+            this.numericUpDown2.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown2.ValueChanged += new System.EventHandler(this.numericUpDown2_ValueChanged);
+            // 
+            // numericUpDown3
+            // 
+            this.numericUpDown3.Location = new System.Drawing.Point(478, 70);
+            this.numericUpDown3.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown3.Name = "numericUpDown3";
+            this.numericUpDown3.Size = new System.Drawing.Size(134, 27);
+            this.numericUpDown3.TabIndex = 12;
+            this.numericUpDown3.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown3.ValueChanged += new System.EventHandler(this.numericUpDown3_ValueChanged);
+            
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(285, 252);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(94, 29);
+            this.button6.TabIndex = 8;
+            this.button6.Text = "Copy2";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.numericUpDown3);
+            this.Controls.Add(this.numericUpDown2);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Chemistry";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.NumericUpDown numericUpDown2;
+        private System.Windows.Forms.NumericUpDown numericUpDown3;
+
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,251 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Chemistry1
+{
+    public partial class Form1 : Form
+    {
+        int number1 = 1;
+        int number2 = 1;
+        int number3 = 1;
+        string result1 = "";
+        string result2 = "";
+        string result3 = "";
+        List<string> formula1 = new List<string> { "H2SO4" };
+        List<string> formula2 = new List<string> { "KOH" };
+        List<string> formula3 = new List<string> { "NaCl" };
+
+        public Form1()
+        {   
+            formula1 = ReadFile1(formula1);
+            formula2 = ReadFile2(formula2);
+            formula3 = ReadFile3(formula3);
+            InitializeComponent();
+        }
+        public List<string> ReadFile1(List<string> textFromFile1)
+        {
+
+            var baseFolder = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+            var relativePath = @"Chem\formula1.txt";
+            var fullPath = Path.Combine(baseFolder, relativePath);
+            textFromFile1 = File.ReadAllLines(fullPath).ToList();
+            return textFromFile1;
+        }
+        public List<string> ReadFile2(List<string> textFromFile2)
+        {
+
+            var baseFolder = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+            var relativePath = @"Chem\formula2.txt";
+            var fullPath = Path.Combine(baseFolder, relativePath);
+            textFromFile2 = File.ReadAllLines(fullPath).ToList();
+            return textFromFile2;
+        }
+        public List<string> ReadFile3(List<string> textFromFile3)
+        {
+
+            var baseFolder = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+            var relativePath = @"Chem\formula3.txt";
+            var fullPath = Path.Combine(baseFolder, relativePath);
+            textFromFile3 = File.ReadAllLines(fullPath).ToList();
+            return textFromFile3;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (number1 == 1)
+            {
+                Random rnd1 = new Random();
+                int ind1 = rnd1.Next(formula1.Count);
+                result1 = formula1[ind1];
+                
+            }
+            else
+            {
+                
+                result1 = "";
+                List<int> check1 = new List<int>();
+                if (number1 > formula1.Count)
+                {
+                    MessageBox.Show("Сильно большое число!");
+                }
+                else
+                {
+                    for (int i = 0; i < number1; i++)
+                    {
+                        if (i == 0)
+                        {
+                            Random rnd1 = new Random();
+                            int ind1 = rnd1.Next(formula1.Count);
+                            result1 += " " + formula1[ind1];
+                            check1.Add(ind1);
+                        }
+                        else
+                        {
+
+                            Random rnd1 = new Random();
+                            int ind1 = rnd1.Next(formula1.Count);
+                            bool findInd = check1.Contains(ind1);
+                            while (findInd == true)
+                            {
+                                ind1 = rnd1.Next(formula1.Count);
+                                findInd = check1.Contains(ind1);
+                            }
+                            check1.Add(ind1);
+                            result1 += " " + formula1[ind1];
+                        }
+                    }
+                }
+            }
+            textBox1.Text = Text = result1;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (number2 == 1)
+            {
+                Random rnd2 = new Random();
+                int ind2 = rnd2.Next(formula2.Count);
+                result2 = formula2[ind2];
+
+            }
+            else
+            {
+                result2 = "";
+                List<int> check2 = new List<int>();
+                if (number2 > formula2.Count)
+                {
+                    MessageBox.Show("Сильно большое число!");
+                }
+                else
+                {
+                    for (int i = 0; i < number2; i++)
+                    {
+                        if (i == 0)
+                        {
+                            Random rnd2 = new Random();
+                            int ind2 = rnd2.Next(formula2.Count);
+                            result2 += " " + formula2[ind2];
+                            check2.Add(ind2);
+                        }
+                        else
+                        {
+
+                            Random rnd2 = new Random();
+                            int ind2 = rnd2.Next(formula2.Count);
+                            bool findInd = check2.Contains(ind2);
+                            while (findInd == true)
+                            {
+                                ind2 = rnd2.Next(formula2.Count);
+                                findInd = check2.Contains(ind2);
+                            }
+                            check2.Add(ind2);
+                            result2 += " " + formula2[ind2];
+                        }
+                    }
+                }
+            }
+            textBox2.Text = Text = result2;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (number3 == 1)
+            {
+                Random rnd3 = new Random();
+                int ind3 = rnd3.Next(formula3.Count);
+                result3 = formula3[ind3];
+
+            }
+            else
+            {
+
+                result3 = "";
+                List<int> check3 = new List<int>();
+                if (number3 > formula3.Count)
+                {
+                    MessageBox.Show("Сильно большое число!");
+                }
+                else
+                {
+                    for (int i = 0; i < number3; i++)
+                    {
+                        if (i == 0)
+                        {
+                            Random rnd3 = new Random();
+                            int ind3 = rnd3.Next(formula3.Count);
+                            result3 += " " + formula3[ind3];
+                            check3.Add(ind3);
+                        }
+                        else
+                        {
+
+                            Random rnd3 = new Random();
+                            int ind3 = rnd3.Next(formula3.Count);
+                            bool findInd = check3.Contains(ind3);
+                            while (findInd == true)
+                            {
+                                ind3 = rnd3.Next(formula3.Count);
+                                findInd = check3.Contains(ind3);
+                            }
+                            check3.Add(ind3);
+                            result3 += " " + formula3[ind3];
+                        }
+                    }
+                }
+            }
+            textBox3.Text = Text = result3;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            result1 = "";
+            result2 = "";
+            result3 = "";
+            textBox1.Text = Text = result1;
+            textBox2.Text = Text = result2;
+            textBox3.Text = Text = result3;
+            numericUpDown1.Value = 1;
+            numericUpDown2.Value = 1;
+            numericUpDown3.Value = 1;
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            Clipboard.SetText(textBox1.Text);
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            Clipboard.SetText(textBox2.Text);
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            Clipboard.SetText(textBox3.Text);
+        }
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+            number1 = (int)numericUpDown1.Value;
+        }
+
+        private void numericUpDown2_ValueChanged(object sender, EventArgs e)
+        {
+            number2 = (int)numericUpDown2.Value;
+        }
+
+        private void numericUpDown3_ValueChanged(object sender, EventArgs e)
+        {
+            number3 = (int)numericUpDown3.Value;
+        }
+    }
+}
